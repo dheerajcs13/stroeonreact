563135484e89f23c14f5890f8a3d4f8d4078a54e
Tekion <dheerajcs13@gmail.com>
2018-04-05 10:47:12 +0530

    check2

--- burgerapp/src/axios-order.js
+++ burgerapp/src/axios-order.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const inst = axios.create({
   baseURL: 'https://burger-app-fc908.firebaseio.com/'
 })
 
-export default instance;
+export default inst;
